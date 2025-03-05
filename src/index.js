8e0efbb92752e0554ffc36ef8a7150f32cc173ae
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-05 12:45:43 -0600

    Understand the subscribing and unsubscribing methods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import store from "./store";
 import { addTask, removeTask } from "./action";
 
+const unsuscribe = store.subscribe(() => {
+    console.log("Updated", store.getState());
+})
+
 store.dispatch(addTask("Learn Redux"));
+store.dispatch(addTask("Learn React"));
 console.log(store.getState());
+
+unsuscribe();
+
 store.dispatch(removeTask(1));
 console.log(store.getState());
