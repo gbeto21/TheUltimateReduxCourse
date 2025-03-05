a86b0afec49dcf449ef6e6dcb69d75f8a319ec2e
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-05 12:30:53 -0600

    Dispatching the Actions from Store

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import store from "./store";
+import { addTask } from "./action";
+
+store.dispatch(addTask("Learn Redux"));
+console.log(store.getState());
+store.dispatch({ type: "REMOVE_TASK", payload: { id: 1 } });
 console.log(store.getState());
