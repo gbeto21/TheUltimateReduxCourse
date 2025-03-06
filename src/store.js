88e294e48adacda1d65cbf1a030c80b5f19d76e8
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-06 10:15:35 -0600

    How to create own middleware

--- src/store.js
+++ src/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./tasks";
 import employeesReducer from "./employees";
+import log from "./middleware/log";
 
 const store = configureStore(
     {
         reducer: {
             tasks: tasksReducer,
             employees: employeesReducer
-        }
+        },
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(log)
     }
 );
 
