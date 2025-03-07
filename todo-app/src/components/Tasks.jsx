46a9322b83c97690d2f4cabe311221a572626b6a
Alberto Gonzalez <albertoglezb@hotmail.com>
2025-03-07 13:04:55 -0600

    Subscribe & Dispatching an action

--- todo-app/src/components/Tasks.jsx
+++ todo-app/src/components/Tasks.jsx
@@ -1,12 +1,25 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import StoreContext from "../contexts/storeContext"
+import { loadTasks } from "../store/tasks"
 
 const Tasks = () => {
 
     const store = useContext(StoreContext)
-    console.log(store);
+    const [tasks, setTasks] = useState([])
 
-    return <div>Tasks</div>
+    useEffect(() => {
+        store.dispatch(loadTasks())
+        const unsuscribe = store.subscribe(() => {
+            const storeTasks = store.getState().tasks.tasks;
+            if (storeTasks !== tasks)
+                setTasks(storeTasks)
+        })
+
+        return () => { unsuscribe() }
+
+    }, [])
+
+    return <div>{tasks.map(task => <p key={task.id}>{task.task}</p>)}</div>
 }
 
 export default Tasks
